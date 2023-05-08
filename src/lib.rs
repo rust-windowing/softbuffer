d862add44c34f4e5c2fc39a7ae973486b757de96
dAxpeDDa <daxpedda@gmail.com>
2023-05-08 12:12:34 +0200

    Document Wasm on docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![warn(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 #[cfg(target_os = "macos")]
 #[macro_use]
