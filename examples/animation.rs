0e6706c7afba582deaa8c47ff049d03cb0f8e363
John Nunley <dev@notgull.net>
2024-05-06 19:25:47 -0700

    ex: Bump winit to v0.30
    This commit bumps winit, which is used in some examples, to version
    v0.30.
    
    This new version comes with many new changes; mainly the move away from
    closures towards applications. In order to work with this, I create a
    simple "winit_app" module that allows the examples to simply and easily
    create winit applications. It takes two closures: one that initializes
    some state on resume, and another that handles events.
    
    I've found this approach reduces diff noise by a lot.
    
    Benchmarks still use deprecated functions for now.
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- examples/animation.rs
+++ examples/animation.rs
@@ -1,89 +1,81 @@
-use instant::Instant;
 #[cfg(not(target_arch = "wasm32"))]
 use rayon::prelude::*;
 use std::f64::consts::PI;
 use std::num::NonZeroU32;
-use std::rc::Rc;
+use web_time::Instant;
 use winit::event::{Event, KeyEvent, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::keyboard::{Key, NamedKey};
-use winit::window::WindowBuilder;
+
+include!("./utils/winit_app.rs");
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let window = Rc::new(WindowBuilder::new().build(&event_loop).unwrap());
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        use winit::platform::web::WindowExtWebSys;
-
-        web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .body()
-            .unwrap()
-            .append_child(&window.canvas().unwrap())
-            .unwrap();
-    }
-
-    let context = softbuffer::Context::new(window.clone()).unwrap();
-    let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
-
-    let mut old_size = (0, 0);
-    let mut frames = pre_render_frames(0, 0);
-
     let start = Instant::now();
-    event_loop
-        .run(move |event, elwt| {
-            elwt.set_control_flow(ControlFlow::Poll);
 
-            match event {
-                Event::WindowEvent {
-                    window_id,
-                    event: WindowEvent::RedrawRequested,
-                } if window_id == window.id() => {
-                    if let (Some(width), Some(height)) = {
-                        let size = window.inner_size();
-                        (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
-                    } {
-                        let elapsed = start.elapsed().as_secs_f64() % 1.0;
+    let app = winit_app::WinitAppBuilder::with_init(|event_loop| {
+        let window = winit_app::make_window(event_loop, |w| w);
 
-                        if (width.get(), height.get()) != old_size {
-                            old_size = (width.get(), height.get());
-                            frames = pre_render_frames(width.get() as usize, height.get() as usize);
-                        };
+        let context = softbuffer::Context::new(window.clone()).unwrap();
+        let surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
 
-                        let frame = &frames[((elapsed * 60.0).round() as usize).clamp(0, 59)];
+        let old_size = (0, 0);
+        let frames = pre_render_frames(0, 0);
 
-                        surface.resize(width, height).unwrap();
-                        let mut buffer = surface.buffer_mut().unwrap();
-                        buffer.copy_from_slice(frame);
-                        buffer.present().unwrap();
-                    }
+        (window, surface, old_size, frames)
+    })
+    .with_event_handler(move |state, event, elwt| {
+        let (window, surface, old_size, frames) = state;
+
+        elwt.set_control_flow(ControlFlow::Poll);
+
+        match event {
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::RedrawRequested,
+            } if window_id == window.id() => {
+                if let (Some(width), Some(height)) = {
+                    let size = window.inner_size();
+                    (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
+                } {
+                    let elapsed = start.elapsed().as_secs_f64() % 1.0;
+
+                    if (width.get(), height.get()) != *old_size {
+                        *old_size = (width.get(), height.get());
+                        *frames = pre_render_frames(width.get() as usize, height.get() as usize);
+                    };
+
+                    let frame = &frames[((elapsed * 60.0).round() as usize).clamp(0, 59)];
+
+                    surface.resize(width, height).unwrap();
+                    let mut buffer = surface.buffer_mut().unwrap();
+                    buffer.copy_from_slice(frame);
+                    buffer.present().unwrap();
                 }
-                Event::AboutToWait => {
-                    window.request_redraw();
-                }
-                Event::WindowEvent {
-                    event:
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            event:
-                                KeyEvent {
-                                    logical_key: Key::Named(NamedKey::Escape),
-                                    ..
-                                },
-                            ..
-                        },
-                    window_id,
-                } if window_id == window.id() => {
-                    elwt.exit();
-                }
-                _ => {}
             }
-        })
-        .unwrap();
+            Event::AboutToWait => {
+                window.request_redraw();
+            }
+            Event::WindowEvent {
+                event:
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                logical_key: Key::Named(NamedKey::Escape),
+                                ..
+                            },
+                        ..
+                    },
+                window_id,
+            } if window_id == window.id() => {
+                elwt.exit();
+            }
+            _ => {}
+        }
+    });
+
+    winit_app::run_app(event_loop, app);
 }
 
 fn pre_render_frames(width: usize, height: usize) -> Vec<Vec<u32>> {
---
+++ examples/utils/winit_app.rs
@@ -0,0 +1,121 @@
+/// Common boilerplate for setting up a winit application.
+mod winit_app {
+    use std::marker::PhantomData;
+    use std::rc::Rc;
+
+    use winit::application::ApplicationHandler;
+    use winit::event::{Event, WindowEvent};
+    use winit::event_loop::{EventLoop, ActiveEventLoop};
+    use winit::window::{WindowAttributes, WindowId, Window};
+
+    /// Run a Winit application.
+    #[allow(unused_mut)]
+    pub(crate) fn run_app(event_loop: EventLoop<()>, mut app: impl ApplicationHandler<()> + 'static) {
+        #[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
+        event_loop.run_app(&mut app).unwrap();
+        
+        #[cfg(any(target_arch = "wasm32", target_arch = "wasm64"))]
+        winit::platform::web::EventLoopExtWebSys::spawn_app(event_loop, app);
+    }
+
+    /// Create a window from a set of window attributes.
+    pub(crate) fn make_window(elwt: &ActiveEventLoop, f: impl FnOnce(WindowAttributes) -> WindowAttributes) -> Rc<Window> {
+        let attributes = f(WindowAttributes::default());
+        #[cfg(target_arch = "wasm32")]
+        let attributes = winit::platform::web::WindowAttributesExtWebSys::with_append(
+            attributes,
+            true
+        );
+        let window = elwt.create_window(attributes);
+        Rc::new(window.unwrap())
+    }
+    
+    /// Easily constructable winit application.
+    pub(crate) struct WinitApp<T, Init, Handler> {
+        /// Closure to initialize state.
+        init: Init,
+
+        /// Closure to run on window events.
+        event: Handler,
+
+        /// Contained state.
+        state: Option<T>
+    }
+
+    /// Builder that makes it so we don't have to name `T`.
+    pub(crate) struct WinitAppBuilder<T, Init> {
+        /// Closure to initialize state.
+        init: Init,
+
+        /// Eat the type parameter.
+        _marker: PhantomData<Option<T>>,
+    }
+
+    impl<T, Init> WinitAppBuilder<T, Init> 
+    where Init: FnMut(&ActiveEventLoop) -> T,
+    {
+        /// Create with an "init" closure.
+        pub(crate) fn with_init(init: Init) -> Self {
+            Self {
+                init,
+                _marker: PhantomData
+            }
+        }
+
+        /// Build a new application.
+        pub(crate) fn with_event_handler<F>(self, handler: F) -> WinitApp<T, Init, F>
+        where F: FnMut(&mut T, Event<()>, &ActiveEventLoop)
+        {
+            WinitApp::new(self.init, handler)
+        }
+    }
+
+    impl<T, Init, Handler> WinitApp<T, Init, Handler> 
+    where Init: FnMut(&ActiveEventLoop) -> T,
+          Handler: FnMut(&mut T, Event<()>, &ActiveEventLoop)
+    {
+        /// Create a new application.
+        pub(crate) fn new(init: Init, event: Handler) -> Self {
+            Self {
+                init,
+                event,
+                state: None
+            }
+        }
+    }
+
+    impl<T, Init, Handler> ApplicationHandler for WinitApp<T, Init, Handler> 
+    where Init: FnMut(&ActiveEventLoop) -> T,
+          Handler: FnMut(&mut T, Event<()>, &ActiveEventLoop) {
+
+        fn resumed(&mut self, el: &ActiveEventLoop) {
+            debug_assert!(self.state.is_none());
+            self.state = Some((self.init)(el));
+        }
+
+        fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
+            let state = self.state.take();
+            debug_assert!(state.is_some());
+            drop(state);
+        }        
+
+        fn window_event(
+            &mut self,
+            event_loop: &ActiveEventLoop,
+            window_id: WindowId,
+            event: WindowEvent,
+        ) {
+            let state = self.state.as_mut().unwrap();
+            (self.event)(state, Event::WindowEvent {
+                window_id,
+                event
+            }, event_loop);
+        }
+
+        fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+            if let Some(state) = self.state.as_mut() {
+                (self.event)(state, Event::AboutToWait, event_loop);
+            }
+        }
+    }
+}
