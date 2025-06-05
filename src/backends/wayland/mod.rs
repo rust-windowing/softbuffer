3f255f43208e88cee7b814d7b0b2eb194aaaac41
Mads Marquart <mads@marquart.dk>
2025-06-05 19:20:31 +0200

    Finish Wayland impl

--- src/backends/wayland/mod.rs
+++ src/backends/wayland/mod.rs
@@ -240,11 +240,15 @@ impl<D: HasDisplayHandle + ?Sized, W: HasWindowHandle> SurfaceInterface<D, W>
             ));
         };
 
+        let width = self.buffers.as_mut().unwrap().1.width;
+        let height = self.buffers.as_mut().unwrap().1.height;
         let age = self.buffers.as_mut().unwrap().1.age;
         Ok(BufferImpl {
             stack: util::BorrowStack::new(self, |buffer| {
                 Ok(unsafe { buffer.buffers.as_mut().unwrap().1.mapped_mut() })
             })?,
+            width,
+            height,
             age,
         })
     }
@@ -259,16 +263,18 @@ impl<D: ?Sized, W: ?Sized> Drop for WaylandImpl<D, W> {
 
 pub struct BufferImpl<'a, D: ?Sized, W> {
     stack: util::BorrowStack<'a, WaylandImpl<D, W>, [u32]>,
+    width: i32,
+    height: i32,
     age: u8,
 }
 
 impl<D: HasDisplayHandle + ?Sized, W: HasWindowHandle> BufferInterface for BufferImpl<'_, D, W> {
     fn width(&self) -> usize {
-        self.stack.member().width
+        self.width as usize
     }
 
     fn height(&self) -> usize {
-        self.stack.member().height
+        self.height as usize
     }
 
     #[inline]
