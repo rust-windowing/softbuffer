5cba2e592f903e29ebeb191ceba148ff3e432bf2
Mads Marquart <mads@marquart.dk>
2026-01-19 16:13:30 +0100

    refactor: Remove BorrowStack
    It is no longer used, and likely won't be necessary in the future
    either.

--- src/util.rs
+++ src/util.rs
@@ -8,57 +8,6 @@ use std::ops;
 
 use crate::Rect;
 
-/// Takes a mutable reference to a container and a function deriving a
-/// reference into it, and stores both, making it possible to get back the
-/// reference to the container once the other reference is no longer needed.
-///
-/// This should be consistent with stacked borrow rules, and miri seems to
-/// accept it at least in simple cases.
-pub struct BorrowStack<'a, T: 'a + ?Sized, U: 'a + ?Sized> {
-    container: *mut T,
-    member: *mut U,
-    _phantom: std::marker::PhantomData<&'a mut T>,
-}
-
-unsafe impl<'a, T: 'a + Send + ?Sized, U: 'a + Send + ?Sized> Send for BorrowStack<'a, T, U> {}
-
-impl<'a, T: 'a + ?Sized, U: 'a + ?Sized> BorrowStack<'a, T, U> {
-    pub fn new<F>(container: &'a mut T, f: F) -> Self
-    where
-        F: for<'b> FnOnce(&'b mut T) -> &'b mut U,
-    {
-        let container = container as *mut T;
-        let member = f(unsafe { &mut *container }) as *mut U;
-        Self {
-            container,
-            member,
-            _phantom: std::marker::PhantomData,
-        }
-    }
-
-    pub fn member(&self) -> &U {
-        unsafe { &*self.member }
-    }
-
-    pub fn member_mut(&mut self) -> &mut U {
-        unsafe { &mut *self.member }
-    }
-
-    pub fn into_container(self) -> &'a mut T {
-        // SAFETY: Since we consume self and no longer reference member, this
-        // mutable reference is unique.
-        unsafe { &mut *self.container }
-    }
-}
-
-impl<'a, T: 'a + ?Sized, U: 'a + ?Sized + fmt::Debug> fmt::Debug for BorrowStack<'a, T, U> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("BorrowStack")
-            .field("member", &self.member())
-            .finish_non_exhaustive()
-    }
-}
-
 /// Calculates the smallest `Rect` necessary to represent all damaged `Rect`s.
 pub(crate) fn union_damage(damage: &[Rect]) -> Option<Rect> {
     struct Region {
@@ -114,23 +63,5 @@ impl ops::Deref for PixelBuffer {
 impl ops::DerefMut for PixelBuffer {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_borrowstack_slice_int() {
-        fn f(mut stack: BorrowStack<[u32], u32>) {
-            assert_eq!(*stack.member(), 3);
-            *stack.member_mut() = 42;
-            assert_eq!(stack.into_container(), &[1, 2, 42, 4, 5]);
-        }
-
-        let mut v = vec![1, 2, 3, 4, 5];
-        f(BorrowStack::new(v.as_mut(), |v: &mut [u32]| &mut v[2]));
-        assert_eq!(&v, &[1, 2, 42, 4, 5]);
     }
 }
